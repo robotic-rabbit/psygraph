09a8f3542e9c1ffc3779834c3ca4c480bd8cb3af
24f2005120 <24f2005120@ds.study.iitm.ac.in>
2026-07-04 17:30:41 +1000

    Added the first schema and api for question submission

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import UserSubmission
 
 app = FastAPI()
 
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Psygraph backend is running normally"}
+
+
+@app.post("/submit")
+def submit_answers(payload: UserSubmission):
+    print(f"Received data for user: {payload.user_id}")
+
+    return {"status": "success", "data_received": payload}
---
+++ backend/schemas.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class SliderAnswer(BaseModel):
+    question_id: int
+    score: float
+
+
+# we might want to change this later but for now this works
+class UserSubmission(BaseModel):
+    user_id: Optional[int] = None
+    answers: List[SliderAnswer]
